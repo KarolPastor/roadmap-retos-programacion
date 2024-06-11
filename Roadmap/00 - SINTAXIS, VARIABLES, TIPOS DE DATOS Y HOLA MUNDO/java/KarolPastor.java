bf2ea8ab65e921f273fcd65ecadf35d10ca480b7
KarolPastor <U22301151@utp.edu.pe>
2024-06-10 22:11:23 -0500

    Create KarolPastor.java

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/KarolPastor.java
@@ -0,0 +1,29 @@
+
+package KarolPastor;
+
+// Comentario en una sola linea//
+// Url: https://www.oracle.com/java/ //
+/* Comentario en
+varias lineas
+*/
+
+public class KarolPastor {
+   
+    // Crear una constante 
+    
+    public static final String LENGUAJE = "Java";
+    
+    public static void main(String[] args) {
+        
+        // Variables primitivas en Java
+        int numeroEntero = 10; // Entero
+        double numeroDecimal = 3.14; // Decimal
+        boolean esVerdadero = true; // Booleano
+        char caracter = 'J'; // Carácter
+        
+        String cadenaDeTexto = "¡Hola, Java!"; // Cadena de texto
+
+        // Imprimir por terminal
+        System.out.println(cadenaDeTexto);
+    }
+}
